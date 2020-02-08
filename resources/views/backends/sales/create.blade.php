b23549db3e6877027a0423a3576a802d09ecdc22
kakrona <39107948+kakrona-yan@users.noreply.github.com>
2020-02-08 13:30:22 +0700

    fix api

--- resources/views/backends/sales/create.blade.php
+++ resources/views/backends/sales/create.blade.php
@@ -195,7 +195,7 @@
         $("#category_id").select2({
             allowClear: false
         }).on('select2:select', function (e) {
-            let routeUrl = "{{config('app.url')}}/admin/sales/product";
+            let routeUrl = "{{config('app.url')}}/sales/product";
             let category_id = e.params.data.id;
             $.ajax({
                 url     : routeUrl,
