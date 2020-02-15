934d4b3f5e904723fc772810f6430cef435d977e
kakrona <39107948+kakrona-yan@users.noreply.github.com>
2020-02-15 11:08:32 +0700

    update price

--- resources/views/backends/sales/create.blade.php
+++ resources/views/backends/sales/create.blade.php
@@ -428,7 +428,7 @@
     }
     // format money
     function formatMoney(money) {
-        return money != '' ? parseFloat(money).toFixed(2) : 0;
+        return money != '' ? Number(money).toFixed(2) : 0;
     }
 </script>
 @endpush
