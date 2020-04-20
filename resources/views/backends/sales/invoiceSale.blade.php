fc2124beef677c480c6882790bbcbca5d5c2a79f
kakrona mx <kakrona@marinax.co.jp>
2020-04-20 08:40:42 +0700

    fix invoice

--- resources/views/backends/sales/invoiceSale.blade.php
+++ resources/views/backends/sales/invoiceSale.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Invoice - #{{ $sale->quotaion_no }}</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
     <!-- Styles -->
@@ -128,6 +129,11 @@
             }
             .con-print {
                 display: block;
+            }
+        }
+        @media (max-width: 999px) {
+            .d-inline{
+                width: 200px;
             }
         }
     </style>
